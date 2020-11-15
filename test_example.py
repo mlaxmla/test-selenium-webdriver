2c6c2d8ac4f9b70dcbab475bbbd029b243a82a11
marcin_laskowski <marcin_laskowski@parp.gov.pl>
2020-11-15 18:21:00 +0100

    Lesson 2 Exercise 1 (python)

---
+++ test_example.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome(executable_path = "D:/Python36/chromedriver.exe")
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://www.google.com/")
+    wait = WebDriverWait(driver, 10) # seconds
+
