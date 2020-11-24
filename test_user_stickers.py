7ba90e1dc09e60de4eb7cec086c5654a8d48d5d7
marcin_laskowski <marcin_laskowski@parp.gov.pl>
2020-11-24 10:25:14 +0100

    ex 8: test_user_stickers v2.extended

--- test_user_stickers.py
+++ test_user_stickers.py
@@ -12,9 +12,14 @@ class TestCaseUserStickers(unittest.TestCase):
     def test_user_stickers(self):
         wd = self.wd
         wd.get("http://localhost/litecart/")
-        stickers_list = wd.find_elements(By.XPATH, "//div[contains(@class,'sticker')]")
-        stickers_list_len = len(stickers_list)
-        wd.implicitly_wait(20)
+        product_list = wd.find_elements(By.XPATH, "//li[contains(@class,'product')]")
+        product_list_len = len(product_list)
+        product_with_stickers_list = wd.find_elements(By.XPATH, "//div[contains(@class,'sticker')]")
+        product_with_stickers_list_len = len(product_with_stickers_list)
+        product_with_1sticker_list = wd.find_elements(By.XPATH, "//a/div[@class='image-wrapper']/img/following-sibling::div[count(preceding-sibling::*)+count(following-sibling::*)=1]")
+        product_with_1sticker_list_len = len(product_with_1sticker_list)
+        assert product_list_len >= product_with_stickers_list_len
+        assert product_with_1sticker_list_len == product_with_stickers_list_len
 
     def tearDown(self):
         self.wd.quit()
