23a086fd679ef46aa7dadb3c549300606f93404d
marcin_laskowski <marcin_laskowski@parp.gov.pl>
2020-11-30 08:07:22 +0100

    ex7: test_admin_menuitems.py (corrected)

--- test_admin_menuitems.py
+++ test_admin_menuitems.py
@@ -11,32 +11,30 @@ class TestCaseAdminMenuItems(unittest.TestCase):
 
     def test_admin_menuitems(self):
         wd = self.wd
+        # 1)    Wejście do panelu administratora http://localhost/litecart/admin.
         wd.get("http://localhost/litecart/admin/login.php")
         wd.find_element_by_name("username").clear()
         wd.find_element_by_name("username").send_keys("admin")
         wd.find_element_by_name("password").clear()
         wd.find_element_by_name("password").send_keys("admin")
         wd.find_element_by_name("login").click()
-        # links_list = wd.find_elements(By.XPATH, "//li[@id='app-']/a/span[@class='name']/..")
-        elements_list = wd.find_elements(By.XPATH, "//li[@id='app-']/a/span[@class='name']")
-        ahrefs_list = wd.find_elements(By.XPATH, "//li[@id='app-']/a/span[@class='name']/..")
+        # 2)    Klikanie kolejno na wszystkie punkty w menu z lewej strony, w tym w zagnieżdżone punkty.
+        ahrefs_list = wd.find_elements(By.XPATH, "//li[@id='app-']//a")
         names_list = []
         hrefs_list = []
-        headers_list = []
-        headers_expected_list = ['Template', 'Catalog', 'Countries', 'Currencies', 'Customers', 'Geo Zones', 'Languages', 'Job Modules', 'Orders', 'Pages', ' Monthly Sales', 'Settings', 'Slides', 'Tax Classes', ' Search Translations', 'Users', 'vQmods']
-        for nameitem, hrefitem, headerexpecteditem in zip(elements_list, ahrefs_list, headers_expected_list):
-            name = nameitem.text
+        for element in ahrefs_list:
+            name = element.find_element_by_class_name("name").text
             names_list.append(name)
-            href = hrefitem.get_attribute("href")
+            href = element.get_attribute("href")
             hrefs_list.append(href)
-            # i don't know how to iterate over these lists in python and go to correct address :(
-#            wd.get(href)
-#            wd.find_element_by_xpath(hrefitem).click()
-#            header = wd.find_element_by_tag_name('H1')
-#            headers_list.append(header)
-#            assert header.text == headerexpecteditem
-#            wd.get("http://localhost/litecart/admin/")
-        return names_list, hrefs_list, headers_list
+        # 3)    Dla każdej strony sprawdzenie nagłówka (to znaczy elementu z tagiem h1).
+        headers_list = []
+        headers_expected_list = ['Template', 'Catalog', 'Countries', 'Currencies', 'Customers', 'Geo Zones', 'Languages', 'Job Modules', 'Orders', 'Pages', 'Monthly Sales', 'Settings', 'Slides', 'Tax Classes', 'Search Translations', 'Users', 'vQmods']
+        for nameitem, hrefitem, headerexpecteditem in zip(names_list, hrefs_list, headers_expected_list):
+            wd.get(hrefitem)
+            header = wd.find_element_by_tag_name('H1').text
+            assert header == headerexpecteditem
+            wd.get("http://localhost/litecart/admin/")
 
     def tearDown(self):
         self.wd.quit()
